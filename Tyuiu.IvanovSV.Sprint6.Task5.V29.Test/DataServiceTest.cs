f7324c92bbb72f9de0e808164530e9f043707c4a
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-12-26 16:53:44 +0500

    Task5

---
+++ Tyuiu.IvanovSV.Sprint6.Task5.V29.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.IvanovSV.Sprint6.Task5.V29.Lib;
+
+namespace Tyuiu.IvanovSV.Sprint6.Task5.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\konev\source\repos\Tyuiu.KonevaDD.Sprint6\Tyuiu.KonevaDD.Sprint6.Task5.V29\bin\Debug\net8.0-windows\InPutDataFileTask5V29.txt";
+            double[] res = ds.LoadFromDataFile(path);
+            double[] wait = { 12, 12.89, 35, 13.83, 12.76 };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.IvanovSV.Sprint6.Task5.V29/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.IvanovSV.Sprint6.Task5.V29
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
