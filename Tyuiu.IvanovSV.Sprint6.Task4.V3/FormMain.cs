22dc3befa4bb211ea47d654306a6bd787ceb4dc6
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-12-26 16:46:35 +0500

    Task4

---
+++ Tyuiu.IvanovSV.Sprint6.Task4.V3/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.IvanovSV.Sprint6.Task4.V3
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
