5e3d587c7044c4de917c869d1946e293954efbe0
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-12-26 17:06:29 +0500

    Task6

--- Tyuiu.IvanovSV.Sprint6.Task6.V16.Lib/DataService.cs
+++ Tyuiu.IvanovSV.Sprint6.Task6.V16.Lib/DataService.cs
@@ -1,7 +1,37 @@
-﻿namespace Tyuiu.IvanovSV.Sprint6.Task6.V16.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
 
+namespace Tyuiu.IvanovSV.Sprint6.Task6.V16.Lib
+{
+    public class DataService : ISprint6Task6V16
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string words = ""; ;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                bool first = true;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] linewords = line.Split(' ');
+                    foreach (string word in linewords)
+                    {
+                        if (word.Contains('b'))
+                        {
+                            if (first == true)
+                            {
+                                words += word;
+                                first = false;
+                            }
+                            else
+                            {
+                                words = words + " " + word;
+                            }
+                        }
+                    }
+                }
+            }
+            return words;
+        }
     }
 }
---
+++ Tyuiu.IvanovSV.Sprint6.Task6.V16.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.IvanovSV.Sprint6.Task6.V16.Lib;
+namespace Tyuiu.IvanovSV.Sprint6.Task6.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint6\InPutFileTask6V16.txt";
+            string res = ds.CollectTextFromFile(path);
+            string wait = "brIBPtX bWk";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
